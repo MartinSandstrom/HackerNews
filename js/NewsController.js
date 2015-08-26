1ae63408348dd893e9b2cb3eb791eb2b6ec2acce
Martin Sandström <maartiin_91@hotmail.com>
2015-08-26 21:13:23 +0200

    Update 2015-08-26T19:13:20.839Z

--- js/NewsController.js
+++ js/NewsController.js
@@ -3,36 +3,58 @@ angular.module("hackerNewsApp", [])
     .controller('newsController', function(NewsService){
 
     var vm = this;
-    var firstTen = [];
-    var numberOfArticles = 10;
+    var articlesToShow = [];
     var currentStorie = '';
     vm.stories = [];
 
     vm.viewMoreItems = function(){
         NewsService.getStories(currentStorie).then(function(response){
-           publishTenStories(response.data, true);
+           publishTenMoreStories(response.data, true);
         });
     }
 
     vm.getStories = function(typeOfStories){
         currentStorie = typeOfStories;
         NewsService.getStories(typeOfStories).then(function(response){
-            publishTenStories(response.data);
+            publishTenMoreStories(response.data);
         });
     }
 
     vm.getStories();
 
-    function publishTenStories(stories, addMore){
-        firstTen = stories.slice(0,10);
-        if(!addMore) vm.stories = [];
+    function publishTenMoreStories(stories, addMore){
+        articlesToShow = [];
+        if(addMore) {
+          var number = 0;
+          var index = 0;
+          while (number < 10) {
+            if(isNew(stories[index])){
+              articlesToShow.push(stories[index])
+              number++;
+            }
+            index++;
+          }
+        }
+        else {
+          articlesToShow = stories.slice(0,10);
+          vm.stories = [];
+        }
 
-        angular.forEach(firstTen, function(id){
+        angular.forEach(articlesToShow, function(id){
             NewsService.getStorie(id).then(function(response){
                 vm.stories.push(response.data);
             });
         });
     }
+
+    function isNew(id){
+      var answer = true;
+      angular.forEach(vm.stories, function(storie){
+        if(storie.id === id) answer = false;
+      });
+      return answer;
+    }
+
 })
 .filter('datediff', function(){
   return function(date) {
