d1e254d6d7a3fda2ec5ddeb0da14400313a3a2be
Martin Sandström <maartiin_91@hotmail.com>
2015-08-27 08:19:46 +0200

    Update 2015-08-27T06:19:44.050Z

--- js/NewsService.js
+++ js/NewsService.js
@@ -1,10 +1,13 @@
 
 angular.module("hackerNewsApp").factory("NewsService", function($http){
+    
+    var defaultStoryType = 'topstories';
+    
     return {
-        getStories: function(typeOfStorie){
-            return $http.get('https://hacker-news.firebaseio.com/v0/' + (typeOfStorie || 'topstories') + '.json');
+        getStories: function(typeOfStory){
+            return $http.get('https://hacker-news.firebaseio.com/v0/' + (typeOfStory || defaultStoryType) + '.json');
         },
-        getStorie: function(id){
+        getStory: function(id){
             return $http.get('https://hacker-news.firebaseio.com/v0/item/' + id  + '.json?print=pretty');
         }
     }
