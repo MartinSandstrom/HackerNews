cfbb3ebec98a821f0c654c5633ac77342c0c0293
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2017-07-10 13:56:15 +0200

    Update 2017-07-10T11:56:12.474Z

--- js/NewsService.js
+++ js/NewsService.js
@@ -1,8 +1,8 @@
 
 angular.module("hackerNewsApp").factory("NewsService", function($http){
-    
+
     var defaultStoryType = 'topstories';
-    
+
     return {
         getStories: function(typeOfStory){
             return $http.get('https://hacker-news.firebaseio.com/v0/' + (typeOfStory || defaultStoryType) + '.json');
@@ -10,5 +10,5 @@ angular.module("hackerNewsApp").factory("NewsService", function($http){
         getStory: function(id){
             return $http.get('https://hacker-news.firebaseio.com/v0/item/' + id  + '.json?print=pretty');
         }
-    }
+    };
 });
